6616836bc7393d06ad8788ef31b39259c2ecbbaa
Shantan Vishwabrahmana <83112794+Shantan243@users.noreply.github.com>
2024-01-09 19:45:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,14 +13,17 @@ def get_demo():
 
 
 # Controller-2
-@app.route("/name", methods=['GET','POST'])
-def get_demo_name():
-    if request.method == 'POST':
-        data = request.get_json()
-        name = data["name"]
-        return jsonify(message="Hello, I am {}!".format(name))
-    else:
-        return jsonify(message="Hello, I am Flask!")
+@app.route("/name/<name>", methods=['GET'])
+def get_demo_name(name):
+    return jsonify(message="Hello, I am {}!".format(name))
+# @app.route("/name", methods=['GET','POST'])
+# def get_demo_name():
+#     if request.method == 'POST':
+#         data = request.get_json()
+#         name = data["name"]
+#         return jsonify(message="Hello, I am {}!".format(name))
+#     else:
+#         return jsonify(message="Hello, I am Flask!")
 
 
 # Running the api
