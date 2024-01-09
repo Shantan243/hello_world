4be5240acdd26331ab0bf31c355746fd8e7816af
Shantan Vishwabrahmana <83112794+Shantan243@users.noreply.github.com>
2024-01-09 18:53:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-import os
+from flask import Flask, request
+import json
 
-from flask import Flask
+# Initializing flask app
 app = Flask(__name__)
-@app.route('/hello_world/<name>')
- 
-def hello_world(name):
-    return "Hello " + name
+
+
+
+# Controller-1
+@app.route("/demo", methods=['GET'])
+def get_demo():
+    return "This is a demo api"
+
+
+# Controller-2
+@app.route("/name", methods=['POST'])
+def get_demo_name():
+    data = request.data
+    body = json.loads(data)
+    name = body["name"]
+    return "Hello, I am {}!".format(name)
+
+
+# Running the api
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
